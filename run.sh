3d3df383f1a3262e5070880ec4f4d5537cb737d7
tukiyo <tukiyo3@gmail.com>
2021-11-30 12:38:34 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
 # privileged を付けないと、google-chromeが一般ユーザーで起動ができない。 /dev/fd/4 への書き込みが失敗などとでた。
 
-docker run --privileged -it --rm -p 80:80 --name=novnc tukiyo3/novnc:latest
-# docker run --privileged -it --rm -p 80:80 --name=novnc tukiyo3/novnc:latest
-# docker run -it --rm -p 5900:5900 --name=novnc tukiyo3/novnc
+docker run -d \
+ --privileged -it \
+ --name=novnc \
+ --hostname=novnc \
+ --restart=always \
+ -w /home/docker \
+ -p 80:80 \
+ -v $(pwd)/files/supervisord.conf:/etc/supervisor/conf.d/supervisord.conf \
+ --name=novnc tukiyo3/novnc:latest
+ 
